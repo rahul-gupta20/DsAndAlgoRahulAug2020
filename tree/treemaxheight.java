54e0e7dc62b7729df0cabb01fe77c0e6e7673521
rahul-gupta20 <69245464+rahul-gupta20@users.noreply.github.com>
2020-09-08 19:49:27 -0700

    Add files via upload

---
+++ tree/treemaxheight.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class treemaxheight{
+    public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+   
+    public static int treeHeight( Node node ){ 
+ 
+         int mht = -1;
+
+        for( int i=0; i<node.children.size(); i++ ){
+            Node child = node.children.get( i );
+            int cheight= treeHeight( child );
+            if( mht < cheight ){
+                mht = cheight;
+            }
+        }
+         return mht + 1;
+    }
+    public static Node construct(int[] arr){
+       Stack<Node> st = new Stack<>();
+       Node root=null;
+       for( int i =0; i<arr.length; i++){
+           if( arr[ i ] == -1){
+               st.pop();
+           }
+           else{
+               Node t = new Node();
+               t.data = arr[ i ];
+               if(st.size()>0){
+                   st.peek().children.add( t );
+               }else{
+                   root = t;
+               }
+               st.push( t );
+           }
+       }
+         return root;
+
+    }
+
+    public static void displaytree(Node node){
+        String str = node.data+" -> ";
+        for(Node child:node.children){
+            str+=child.data+",";
+        }
+        str+=".";
+        System.out.println( str );
+         for(Node child:node.children){
+             displaytree(child);
+         }
+    }
+    public static void main(String[] args){
+      int[] arr = { 10 , 20 , -1 , 30 , 50 , -1, 60 , -1 ,-1,40,-1,-1 };
+        Node root = construct( arr );
+        // displaytree(root);
+        int ans = treeHeight( root );
+        System.out.println( ans );
+    }
+}
